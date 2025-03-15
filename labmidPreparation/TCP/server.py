0a156e47a997b04492860e4d88d7e3026de33057
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-03-15 10:43:11 +0500

    TCP connection done

---
+++ labmidPreparation/TCP/server.py
@@ -0,0 +1,24 @@
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+ server_address=('localhost',10000)
+ sock.bind(server_address)
+ sock.listen(1)
+ while True:
+    try:
+        connection,address=sock.accept()
+        while True:
+         data=connection.recv(16)
+         if data:
+           print("Data from client is ",data.decode())
+         else:
+           
+            break  
+        data="asda dad adda dasda dadssada dawer rwrw rrdsfdg gdfhh weretadsg "
+        connection.sendall(data.encode())
+        connection.shutdown(socket.SHUT_WR)
+    except Exception as e:
+     print(e)
+
+
+ 
